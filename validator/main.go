2759a9a82aa8160b22b8f15a792c181a56f73c82
najam sikander awan <najamsk@gmail.com>
2022-02-10 19:34:03 +0200

    well need to do few things

--- validator/main.go
+++ validator/main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
+	//XXX: Check mario recommended validator package
 	fmt.Println("hello world")
 }
---
+++ web_frameworks/echo/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/users/:id", getUser)
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.POST("/users", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		e.Logger.Print("user is =", u)
+
+		return c.JSON(http.StatusCreated, u)
+		// or
+		// return c.XML(http.StatusCreated, u)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+
+}
+
+// e.GET("/users/:id", getUser)
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
