fd072670110dc5987d2f0a831b7e0f802ac2bc28
najam awan <najamsk@gmail.com>
2022-11-15 15:39:36 +0200

    leetcode 1647 added

---
+++ CodeWars/60.MinDelToMakeFrequenciesUnique/1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func test(s string) int {
+	m := make(map[string]int, len(s))
+	l := 0
+
+	for _, v := range s {
+		f := m[string(v)] + 1
+		l = int(math.Max(float64(f), float64(l)))
+		m[string(v)] = f
+	}
+
+	// fmt.Println(m)
+	// fmt.Println(l)
+	dels := 0
+	for _, v := range m {
+		if v > l {
+			dels++
+		}
+		l--
+	}
+
+	return dels
+}
+
+func main() {
+	fmt.Println(test("aabbbcca"))
+	fmt.Println(test("aab"))
+}
