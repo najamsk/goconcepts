137fb36555194c45bf8441d87f84b6b80264c229
najam awan <najamsk@gmail.com>
2023-01-10 19:42:41 +0200

    updated calledBack functions with closure

--- callbackFunctions/1.go
+++ callbackFunctions/1.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
 func greet(s string, ff func(string) string) string {
 	return ff(s)
@@ -13,8 +17,25 @@ func closuer() func(string) string {
 	}
 }
 
+func StartTimer(name string) func() {
+	t := time.Now()
+	log.Println(name, "started")
+	return func() {
+		d := time.Since(t)
+		log.Println(name, "took", d)
+	}
+
+}
+
+func funcyFunction() {
+	stop := StartTimer("funcyFunc")
+	defer stop()
+	time.Sleep(1 * time.Second)
+}
+
 func main() {
 	dr := closuer()
 	vv := greet("najam awan", dr)
 	fmt.Println(vv)
+	funcyFunction()
 }
